811a4605cb5d31216aa027c48b3ada830adf1bab
staymanHou <hhyycc0418@gmail.com>
2014-01-23 21:44:51 -0500

    half way

--- HW1.rb
+++ HW1.rb
@@ -3,18 +3,18 @@ def test(name, input, exp_output, your_output)
 	puts(name + " ===================")
 	puts("input: " + input)
 	puts("expected output: " + exp_output.to_s + "| your_output: " + your_output.to_s)
-	puts(exp_output == your_output ? "Correct!" : "[Wrong!]")
+	puts(exp_output == your_output ? "+++++++ Correct!" : "------- Wrong!")
 end
 
 # Homework 1
 # 1. Check the documentation on Array, Hash and Enumerable as they could help tremendously with these exercises. :-)
 # Define a method sum which takes an array of integers as an argument and returns the sum of its elements. For an empty array it should return zero.
 def sum(i_array)
-	s = 0
+	@s = 0
 	i_array.each do |i|
-		s += i
+		@s += i
 	end
-	return s
+	return @s
 end
 
 test("sum #1", "[1,2,3,-1]", 5, sum([1,2,3,-1]))
@@ -22,7 +22,18 @@ test("sum #2", "[]", 0, sum([]))
 
 # Define a method max_2_sum which takes an array of integers as an argument and returns the sum of its two largest elements. For an empty array it should return zero. For an array with just one element, it should return that element.
 def max_2_sum(i_array)
-	# Code Here
+	@top = [0, 0]
+	i_array.each do |i|
+		if i > @top[0]
+			if i > @top[1]
+				@top[0] = @top[1]
+				@top[1] = i
+			else
+				@top[0] = i
+			end
+		end
+	end
+	return sum(@top)
 end
 
 test("max_2_sum #1", "[1,2,3,-1]", 5, max_2_sum([1,2,3,-1]))
@@ -31,7 +42,15 @@ test("max_2_sum #3", "[]", 0, max_2_sum([]))
 
 # Define a method sum_to_n? which takes an array of integers and an additional integer, n, as arguments and returns true if any two elements in the array of integers sum to n. An empty array should sum to zero by definition.
 def sum_to_n?(i_array, n)
-	# Code Here
+	if i_array.length == 0
+		return n == 0 ? true : false
+	end
+	while i = i_array.shift
+		if i_array.include?(n-i)
+			return true
+		end
+	end
+	return false
 end
 
 test("sum_to_n? #1", "[1,2,3], 2", false, sum_to_n?([1,2,3], 2))
@@ -52,9 +71,9 @@ def starts_with_consonant?(s)
 	# Code Here
 end
 
-test("starts_with_consonant(s) #1", "\"Amazing\"", false, starts_with_consonant("Amazing"))
-test("starts_with_consonant(s) #2", "\"grace\"", true, starts_with_consonant("grace"))
-test("starts_with_consonant(s) #3", "\"!!!\"", true, starts_with_consonant("!!!"))
+test("starts_with_consonant(s) #1", "\"Amazing\"", false, starts_with_consonant?("Amazing"))
+test("starts_with_consonant(s) #2", "\"grace\"", true, starts_with_consonant?("grace"))
+test("starts_with_consonant(s) #3", "\"!!!\"", true, starts_with_consonant?("!!!"))
 
 # Define a method binary_multiple_of_4?(s) that takes a string and returns true if the string represents a binary number that is a multiple of 4. NOTE: be sure it returns false if the string is not a valid binary number!
 def binary_multiple_of_4?(s)
